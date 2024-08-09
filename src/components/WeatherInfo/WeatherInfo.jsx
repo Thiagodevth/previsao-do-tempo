78ffaf48e565b746a35dff31404ec2cf10a86586
Thiago fidelis <139066077+Thiagodevth@users.noreply.github.com>
2024-08-08 23:30:38 -0300

    fazendo alguns ajsutes no weatherInfo

--- src/components/WeatherInfo/WeatherInfo.jsx
+++ src/components/WeatherInfo/WeatherInfo.jsx
@@ -2,25 +2,25 @@
 import './WeatherInfo.css'
 
 function WeatherInformation({ weather }) {
-
-    return (
-        <div className='weather-container'>
-            <div className='weather-car'>
-                <h2>{weather.name}</h2>
-                <div className='weather-info'>
-                    <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt="imagem-icon" />
-                    <p className='temperature'>{Math.round(weather.main.temp)}ºC</p>
-                </div>
-                <p className='description'>{weather.weather[0].description}</p>
-
-            </div>
-            <div className='details'>
-                <p className='details-itens'>Sensação témica: {Math.round(weather.main.feels_like)}ºC</p>
-                <p className='details-itens'>Umidade: {weather.main.humidity}%</p>
-                <p className='details-itens'>Pressão: {weather.main.pressure}</p>
-            </div>
+  console.log(weather)
+  return (
+    <div className='weather-container'>
+      <div className='weather-car'>
+        <h2>{weather.name}</h2>
+        <div className='weather-info'>
+          <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}.png`} alt="imagem-icon" />
+          <p className='temperature'>{Math.round(weather.main.temp)}ºC</p>
         </div>
-    )
+        <p className='description'>{weather.weather[0].description}</p>
+
+      </div>
+      <div className='details'>
+        <p className='details-itens'>Sensação témica: {Math.round(weather.main.feels_like)}ºC</p>
+        <p className='details-itens'>Umidade: {weather.main.humidity}%</p>
+        <p className='details-itens'>Pressão: {weather.main.pressure}</p>
+      </div>
+    </div>
+  )
 }
 
 export default WeatherInformation
